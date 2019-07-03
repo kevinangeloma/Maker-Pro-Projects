b6dea83ea0c539a5a76af46007c6b5b085236176
kevinangeloma <52477996+kevinangeloma@users.noreply.github.com>
2019-07-03 11:08:04 +0800

    Add files via upload

---
+++ desktop clock/clock.py
@@ -0,0 +1,150 @@
+
+
+import RPi.GPIO as GPIO  #calling for header file which helps in using GPIO’s of PI
+import time            #we are calling for time to provide delays in program
+import datetime        #we are calling for DATE
+
+from DS3231 import SDL_DS3231 as DS3231 #calling for special functions which helps us interface RTC module
+from board import *
+
+h=0                    #integers for storing values
+m=0
+alarm=0
+string_of_characters = 0
+
+rtc = DS3231.SDL_DS3231(1,0x68)
+rtc.write_now()
+
+GPIO.setwarnings(False)  #do not show any warnings
+GPIO.setmode (GPIO.BCM)    #programming the GPIO by BCM pin numbers. (like PIN29 as'GPIO5')
+
+#initialize GPIO17,27,24,23,18,26,5,6,13,19 as an output
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(26,GPIO.OUT)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(6,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(19,GPIO.OUT)
+
+GPIO.setup(21,GPIO.IN)  #initialize GPIO21 as an input.
+GPIO.setup(20,GPIO.IN)  #initialize GPIO20 as an input.
+GPIO.setup(16,GPIO.IN) 
+GPIO.setup(12,GPIO.IN) 
+GPIO.setup(25,GPIO.IN) 
+
+GPIO.setup(22,GPIO.OUT) #initialize GPIO22 as an output.
+
+def send_a_command (command):  #steps for sending a command to 16*2LCD
+    pin=command
+    PORT(pin);
+    GPIO.output(17,GPIO.LOW)
+    GPIO.output(27,GPIO.HIGH)
+    time.sleep(0.001)
+    GPIO.output(27,GPIO.LOW)
+    pin=0
+    PORT(pin); 
+
+def send_a_character (character):#steps for sending a character to 16*2 LCD
+    pin=character
+    PORT(pin);
+    GPIO.output(17,GPIO.HIGH)
+    GPIO.output(27,GPIO.HIGH)
+    time.sleep(0.001)
+    GPIO.output(27,GPIO.LOW)
+    pin=0
+    PORT(pin);
+
+def PORT(pin):            #assigning level for PI GPIO for sending data to LCD through D0-D7
+    if(pin&0x01 == 0x01):
+        GPIO.output(24,GPIO.HIGH)
+    else:
+        GPIO.output(24,GPIO.LOW)
+    if(pin&0x02 == 0x02):
+        GPIO.output(23,GPIO.HIGH)
+    else:
+        GPIO.output(23,GPIO.LOW)
+    if(pin&0x04 == 0x04):
+        GPIO.output(18,GPIO.HIGH)
+    else:
+        GPIO.output(18,GPIO.LOW)
+    if(pin&0x08 == 0x08):
+        GPIO.output(26,GPIO.HIGH)
+    else:
+        GPIO.output(26,GPIO.LOW)    
+    if(pin&0x10 == 0x10):
+        GPIO.output(5,GPIO.HIGH)
+    else:
+        GPIO.output(5,GPIO.LOW)
+    if(pin&0x20 == 0x20):
+        GPIO.output(6,GPIO.HIGH)
+    else:
+        GPIO.output(6,GPIO.LOW)
+    if(pin&0x40 == 0x40):
+        GPIO.output(13,GPIO.HIGH)
+    else:
+        GPIO.output(13,GPIO.LOW)
+    if(pin&0x80 == 0x80):
+        GPIO.output(19,GPIO.HIGH)
+    else:
+        GPIO.output(19,GPIO.LOW)
+
+def send_a_string(string_of_characters):  #steps for sending string of characters to LCD
+  string_of_characters = string_of_characters.ljust(16," ")
+  for i in range(16):
+    send_a_character(ord(string_of_characters[i])) 
+#send characters one by one until all the strings characters are sent through data port
+
+while True: 
+    send_a_command(0x38); #use two lines of LCD
+    send_a_command(0x0E); #screen and cursor ON
+    send_a_command(0x01); #clear screen
+    time.sleep(0.1)       #sleep for 100msec
+    while True:
+        if (GPIO.input(21) == 0):
+            if (h<23):    #if button1 is pressed and hour count is less than 23 increment 'h' by one
+                h=h+1
+
+        if (GPIO.input(20) == 0):
+            if (h>0):     #if button2 is pressed and hour count is more than 0 decrease 'h' by one
+                h=h-1
+
+        if (GPIO.input(16) == 0):
+            if (m<59):    #if button3 is pressed and minute count is less than 59 increment 'm' by one
+                m=m+1
+
+        if (GPIO.input(12) == 0):
+            if (m>0):     #if button4is pressed and minute count is more than 0 decrease 'm' by one
+                m=m-1
+
+        if (GPIO.input(25) == 0):  #if button5 is pressed toggle Alarm ON and OFF
+            if (alarm==0):
+                alarm=1
+            else:
+                alarm=0
+            time.sleep(0.1)
+
+        if (alarm==1):
+            send_a_command(0x80 + 0x40 + 12);
+            send_a_string("ON");  #if alarm is set, then display "ON" at the 12th position of second line of LCD
+
+            if ((h==rtc._read_hours())):
+                if ((m==rtc._read_minutes())):
+                    GPIO.output(22,1)  #if alarm is set, and hour-minute settings match the RTC time, trigger the buzzer
+                else:
+                    GPIO.output(22,0)
+
+        if (alarm==0):
+            send_a_command(0x80 + 0x40 + 12);
+            send_a_string("OFF"); #if alarm is OFF, then display "OFF" at the 12th position of second line of LCD
+            GPIO.output(22,0)       #turn off the buzzer         
+
+        send_a_command(0x80 + 0);   #move courser to 0 position
+        send_a_string ("Time:%02d:%02d:%02d" % (rtc._read_hours(),rtc._read_minutes(),rtc._read_seconds()));
+        #display RTC hours, minutes, seconds
+        send_a_command(0x80 + 0x40 + 0);  #move courser to second line
+        send_a_string ("Alarm:%02d:%02d" % (h,m));  #show alarm time
+        time.sleep(0.1)  #wait for 100msec
---
+++ medicine reminder/medicine scheduler.py
@@ -0,0 +1,137 @@
+from time import sleep, strftime,localtime
+from datetime import datetime
+import RPi.GPIO as GPIO
+import Adafruit_CharLCD as LCD
+import DS3231 import SDL_DS3231 as DS3231
+
+time_window_size = 5 #duration of the reminder in minutes
+schedule = [1080,1142,1200] #list of scheduled medicine reminders
+#schedule is kept in minutes past midnight
+
+# Raspberry Pi pin setup
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 2
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows = 2
+
+# Define pin number of specific day
+sunday = 27
+monday = 5
+tuesday = 6
+wednesday = 26
+thursday = 16
+friday = 12
+saturday = 13
+
+# Mode can either be BOARD or BCM
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarning(False)  
+
+# Iniatilization of GPIO pins
+
+GPIO.setup(sunday, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(monday, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(tuesday, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(wednesday, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(thursday, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(friday, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(saturday, GPIO.OUT, initial=GPIO.LOW)
+
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+was_printed = False
+off_was_printed = True
+one_time_only = False
+checked_schedule = False
+run = True
+
+while run:
+	lcd.clear()
+	message = (datetime.now().strftime('%b %d %Y \n%I:%M:%S %p\n'))
+	lcd.message(message)
+	sleep(1)
+	dt = list(time.localtime())
+	hour = dt[3]
+	minute = dt[4]
+	second = dt[5]
+	weekday = dt[6] # Monday is 0
+
+	if second < 10: #check schedule once a minute only
+		if one_time_only == False:
+			one_time_only = True
+			checked_schedule = False
+	else:
+    	one_time_only = 0
+
+	cur_time_mins = minute + 60 * hour # current time in minutes past midnight
+        
+	if checked_schedule == False:
+    	if was_printed == False:
+        	checked_schedule = True
+        	schedule_loop = 0
+     		while schedule_loop < len(schedule):
+            	schedule_time_window = schedule[schedule_loop] + time_window_size
+            	if schedule_time_window > (59 + 23*60):
+                	schedule_time_window = (59 + 23*60)
+            	if cur_time_mins < schedule_time_window:
+                	if schedule[schedule_loop] <= cur_time_mins:
+                		print("Medicine Alarm!!")
+                    	was_printed = True;
+                    	turn_off_mins = schedule[schedule_loop] + time_window_size
+                    	off_was_printed = False
+                    	if turn_off_mins > (59 + 23*60): # if turn off past midnight
+                    		turn_off_mins = (59 + 23*60) #   then turn off one minute before midnight
+						if weekday == 0:
+                        	GPIO.output(monday, GPIO.HIGH)
+                    	elif weekday == 1:
+                        	GPIO.output(tuesday, GPIO.HIGH)
+                    	elif weekday == 2:
+                        	GPIO.output(wednesday, GPIO.HIGH)
+                    	elif weekday == 3:
+                        	GPIO.output(thursday, GPIO.HIGH)
+                    	elif weekday == 4:
+                        	GPIO.output(friday, GPIO.HIGH)
+                    	elif weekday == 5:
+                        	GPIO.output(saturday, GPIO.HIGH)
+                    	elif weekday == 6:
+                        	GPIO.output(sunday, GPIO.HIGH)
+                        lcd.clear()
+                        message = 'Its Time!!'
+                        lcd.message(message)
+                        n=0
+                        while (n<12):
+                        	for i in range(lcd_columns-len(message)):
+                        		sleep(0.5)
+                        		lcd.move.right()
+                        	for i in range(lcd_columns-len(message)):
+                        		sleep(0.5)
+                        		lcd.move_left()
+                        	n += 1
+            	schedule_loop += 1
+        if off_was_printed == False:
+            if cur_time_mins >= turn_off_mins:
+            	print("Medicine Alarm Off!")
+                off_was_printed = True
+                was_printed = 0 # to prevent re-checking during the reminder period that would move the reminder window and we do not want that
+                if weekday == 0:
+                    GPIO.output(monday, GPIO.LOW)
+                elif weekday == 1:
+                    GPIO.output(tuesday, GPIO.LOW)
+                elif weekday == 2:
+                    GPIO.output(wednesday, GPIO.LOW)
+                elif weekday == 3:
+                    GPIO.output(thursday, GPIO.LOW)
+                elif weekday == 4:
+                    GPIO.output(friday, GPIO.LOW)
+                elif weekday == 5:
+                    GPIO.output(saturday, GPIO.LOW)
+                elif weekday == 6:
+                    GPIO.output(sunday, GPIO.LOW)
