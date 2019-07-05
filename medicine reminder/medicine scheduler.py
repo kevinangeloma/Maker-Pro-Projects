e1d91ebb3ff8e53c3a34264f3057a291b8a81419
kevinangeloma <kma@innovuze.com>
2019-07-05 16:34:52 +0100

    update

--- medicine reminder/medicine scheduler.py
+++ medicine reminder/medicine scheduler.py
@@ -1,8 +1,8 @@
 from time import sleep, strftime,localtime
 from datetime import datetime
 import RPi.GPIO as GPIO
-import Adafruit_CharLCD as LCD
-import DS3231 import SDL_DS3231 as DS3231
+from adafruit_character_lcd import character_lcd  as LCD
+from DS3231 import SDL_DS3231 as DS3231
 
 time_window_size = 5 #duration of the reminder in minutes
 schedule = [1080,1142,1200] #list of scheduled medicine reminders
@@ -32,7 +32,7 @@ saturday = 13
 
 # Mode can either be BOARD or BCM
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarning(False)  
+GPIO.setwarning(False)
 
 # Iniatilization of GPIO pins
 
@@ -52,86 +52,83 @@ off_was_printed = True
 one_time_only = False
 checked_schedule = False
 run = True
-
 while run:
-	lcd.clear()
-	message = (datetime.now().strftime('%b %d %Y \n%I:%M:%S %p\n'))
-	lcd.message(message)
-	sleep(1)
-	dt = list(time.localtime())
-	hour = dt[3]
-	minute = dt[4]
-	second = dt[5]
-	weekday = dt[6] # Monday is 0
+  lcd.clear()
+  message = (datetime.now().strftime('%b %d %Y \n%I:%M:%S %p\n'))
+  lcd.message(message)
+  sleep(1)
+  dt = list(time.localtime())
+  hour = dt[3]
+  minute = dt[4]
+  second = dt[5]
+  weekday = dt[6] # Monday is 0
 
-	if second < 10: #check schedule once a minute only
-		if one_time_only == False:
-			one_time_only = True
-			checked_schedule = False
-	else:
-    	one_time_only = 0
-
-	cur_time_mins = minute + 60 * hour # current time in minutes past midnight
-        
-	if checked_schedule == False:
-    	if was_printed == False:
-        	checked_schedule = True
-        	schedule_loop = 0
-     		while schedule_loop < len(schedule):
-            	schedule_time_window = schedule[schedule_loop] + time_window_size
-            	if schedule_time_window > (59 + 23*60):
-                	schedule_time_window = (59 + 23*60)
-            	if cur_time_mins < schedule_time_window:
-                	if schedule[schedule_loop] <= cur_time_mins:
-                		print("Medicine Alarm!!")
-                    	was_printed = True;
-                    	turn_off_mins = schedule[schedule_loop] + time_window_size
-                    	off_was_printed = False
-                    	if turn_off_mins > (59 + 23*60): # if turn off past midnight
-                    		turn_off_mins = (59 + 23*60) #   then turn off one minute before midnight
-						if weekday == 0:
-                        	GPIO.output(monday, GPIO.HIGH)
-                    	elif weekday == 1:
-                        	GPIO.output(tuesday, GPIO.HIGH)
-                    	elif weekday == 2:
-                        	GPIO.output(wednesday, GPIO.HIGH)
-                    	elif weekday == 3:
-                        	GPIO.output(thursday, GPIO.HIGH)
-                    	elif weekday == 4:
-                        	GPIO.output(friday, GPIO.HIGH)
-                    	elif weekday == 5:
-                        	GPIO.output(saturday, GPIO.HIGH)
-                    	elif weekday == 6:
-                        	GPIO.output(sunday, GPIO.HIGH)
-                        lcd.clear()
-                        message = 'Its Time!!'
-                        lcd.message(message)
-                        n=0
-                        while (n<12):
-                        	for i in range(lcd_columns-len(message)):
-                        		sleep(0.5)
-                        		lcd.move.right()
-                        	for i in range(lcd_columns-len(message)):
-                        		sleep(0.5)
-                        		lcd.move_left()
-                        	n += 1
-            	schedule_loop += 1
-        if off_was_printed == False:
-            if cur_time_mins >= turn_off_mins:
-            	print("Medicine Alarm Off!")
-                off_was_printed = True
-                was_printed = 0 # to prevent re-checking during the reminder period that would move the reminder window and we do not want that
+  if second < 10: #check schedule once a minute only
+    if one_time_only == False:
+      one_time_only = True
+      checked_schedule = False
+    else:
+      one_time_only = 0
+    cur_time_mins = minute + 60 * hour # current time in minutes past midnight
+    if checked_schedule == False:
+        if was_printed == False:
+          checked_schedule = True
+          schedule_loop = 0
+          while schedule_loop < len(schedule):
+            schedule_time_window = schedule[schedule_loop] + time_window_size
+            if schedule_time_window > (59 + 23*60):
+              schedule_time_window = (59 + 23*60)
+            if cur_time_mins < schedule_time_window:
+              if schedule[schedule_loop] <= cur_time_mins:
+                print("Medicine Alarm!!")
+                was_printed = True;
+                turn_off_mins = schedule[schedule_loop] + time_window_size
+                off_was_printed = False
+                if turn_off_mins > (59 + 23*60): # if turn off past midnight
+                  turn_off_mins = (59 + 23*60) #   then turn off one minute before midnight
                 if weekday == 0:
-                    GPIO.output(monday, GPIO.LOW)
+                        GPIO.output(monday, GPIO.HIGH)
                 elif weekday == 1:
-                    GPIO.output(tuesday, GPIO.LOW)
+                        GPIO.output(tuesday, GPIO.HIGH)
                 elif weekday == 2:
-                    GPIO.output(wednesday, GPIO.LOW)
+                        GPIO.output(wednesday, GPIO.HIGH)
                 elif weekday == 3:
-                    GPIO.output(thursday, GPIO.LOW)
+                        GPIO.output(thursday, GPIO.HIGH)
                 elif weekday == 4:
-                    GPIO.output(friday, GPIO.LOW)
+                        GPIO.output(friday, GPIO.HIGH)
                 elif weekday == 5:
-                    GPIO.output(saturday, GPIO.LOW)
+                        GPIO.output(saturday, GPIO.HIGH)
                 elif weekday == 6:
-                    GPIO.output(sunday, GPIO.LOW)
+                        GPIO.output(sunday, GPIO.HIGH)
+                lcd.clear()
+                message = 'Its Time!!'
+                lcd.message(message)
+                n=0
+                while (n<12):
+                  for i in range(lcd_columns-len(message)):
+                    sleep(0.5)
+                    lcd.move.right()
+                for i in range(lcd_columns-len(message)):
+                  sleep(0.5)
+                  lcd.move_left()
+                n += 1
+            schedule_loop += 1
+        if off_was_printed == False:
+          if cur_time_mins >= turn_off_mins:
+            print("Medicine Alarm Off!")
+            off_was_printed = True
+            was_printed = 0 # to prevent re-checking during the reminder period that would move the reminder window and we do not wan$
+            if weekday == 0:
+              GPIO.output(monday, GPIO.LOW)
+            elif weekday == 1:
+              GPIO.output(tuesday, GPIO.LOW)
+            elif weekday == 2:
+              GPIO.output(wednesday, GPIO.LOW)
+            elif weekday == 3:
+              GPIO.output(thursday, GPIO.LOW)
+            elif weekday == 4:
+              GPIO.output(friday, GPIO.LOW)
+            elif weekday == 5:
+              GPIO.output(saturday, GPIO.LOW)
+            elif weekday == 6:
+              GPIO.output(sunday, GPIO.LOW)
